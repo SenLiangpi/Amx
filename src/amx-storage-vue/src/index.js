b6c37d5695701d008e8b5410e32f2d7875794955
pisenliang <pisenliang@gmail.com>
2020-10-22 19:42:18 +0800

    改版

--- src/amx-storage-vue/src/index.js
+++ src/amx-storage-vue/src/index.js
@@ -1,201 +1,48 @@
 /*
- * @Author: PiPi
- * @Github: https://github.com/SenLiangpi
+ * @Author: Pi Patle
  * @Email: pisenliang@gmail.com
- * @Date: 2019-06-17 15:37:41
- * @LastEditors: PiPi
- * @LastEditTime: 2020-03-19 12:43:02
+ * @Github: https://github.com/SenLiangpi
+ * @Website: https://senliangpi.github.io/blog/#/
+ * @Date: 2020-10-21 10:29:36
+ * @LastEditors: Pi Patle
+ * @LastEditTime: 2020-10-21 16:52:03
  */
-// localStorage 写入操作 监听
-let orignalSetItem = localStorage.setItem;
-localStorage.setItem = function (key, newValue) {
-  let setItemEvent = new Event("setItemEvent")
-  setItemEvent.key = key
-  setItemEvent.newValue = newValue
-  window.dispatchEvent(setItemEvent)
-  orignalSetItem.apply(this, arguments)
-}
-//监听 localStorage 改变
-window.addEventListener('storage', (e) => {
-  if(window.amx[e.key] != e.newValue)
+const amx_storage_vue = {};
+//深度監聽 json
+const dataGet = (value)=>{
+  let data
   try {
-    window.amx[e.key] = recursion(JSON.parse(e.newValue),function () {
-      if (localStorage.getItem(e.key)) {
-        localStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
-      } else if (sessionStorage.getItem(e.key)) {
-        sessionStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
-      }
-    })
+    data = JSON.parse(value)
   } catch (e) {
-    window.amx[e.key] = e.newValue
+    data = value
   }
-})
-//监听 localStorage 写入
-window.addEventListener("setItemEvent", (e) => {
-  if(JSON.stringify(window.amx[e.key]) != e.newValue){
-    try {
-      window.amx[e.key] = recursion(JSON.parse(e.newValue),function () {
-        if (localStorage.getItem(e.key)) {
-          localStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
-        } else if (sessionStorage.getItem(e.key)) {
-          sessionStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
-        }
-      })
-    } catch (e) {
-      window.amx[e.key] = e.newValue
-    }
-  }
-});
-
-// 深度监听 json
-function recursion(obj, Callback) {
-  var num = {}
-  function x(o, z) {
-    for (let a in o) {
-      let value = o[a], voType
-      Object.defineProperty(z, a, {
-        enumerable: true,
-        configurable: true,
-        get: function () {
-          return value
-        },
-        set: function (v) {
-          value = v
-          Callback()
-        }
-      })
-      try {
-        if(o[a].constructor.name == 'Object'){
-          x(o[a],z[a])
-        }
-      } catch (e) {
-        // console.log(e)
-      }
-    }
-  }
-  try {
-    if(obj.constructor.name == 'Object'){
-      x(obj, num)
-    }
-  } catch (e) {
-    // console.log(e)
-  }
-  return num
-}
-// 深度监听一个 localStorage
-function keyData(key) {
-  var value = {}, voType
-  try {
-    value[key] = JSON.parse(dataGet(key))
-  } catch (e) {
-    value[key] = dataGet(key)
-  }
-  try {
-    voType = value[key].constructor.name
-  } catch (e) {
-    voType = ''
-  }
-  if (voType == 'Object') {
-    value[key] = recursion(value[key], function () {
-      if (localStorage.getItem(key)) {
-        localStorage.setItem(key, JSON.stringify(window.amx[key]))
-      } else if (sessionStorage.getItem(key)) {
-        sessionStorage.setItem(key, JSON.stringify(window.amx[key]))
-      }
-    })
-  }
-  Object.defineProperty(window.amx, key, {
-    enumerable: true,
-    configurable: true,
-    get: function () {
-      return value[key]
-    },
-    set: function (v) {
-      value[key] = v
-      if (localStorage.getItem(key)) {
-        localStorage.setItem(key, JSON.stringify(v))
-      } else if (sessionStorage.getItem(key)) {
-        sessionStorage.setItem(key, JSON.stringify(v))
-      }
-    }
-  })
+  return data
 }
 
-function dataGet(key) {
-  const type1 = localStorage.getItem(key), type2 = sessionStorage.getItem(key)
-  if (!type1 && !type2) {
-    return {
-      data() {
-        return {
-          key: 'name repeat'
-        }
-      }
+amx_storage_vue.install = (Vue, todos)=>{
+  console.log(Vue)
+  console.log(todos)
+  for(let todo in todos.local){
+    if (!localStorage.getItem(todo)) {
+      localStorage.setItem(todo, JSON.stringify(todos.local[todo]))
     }
   }
-  if (type1) {
-    return type1
-  }
-  if (type2) {
-    return type2
-  }
-}
-let Toast = {}
-Toast.install = function (Vue, todos) {
-  window.amx = {}
-  if (todos.data) {
-    for (let todo in todos.data) {
-      if (!localStorage.getItem(todo) && !sessionStorage.getItem(todo)) {
-        localStorage.setItem(todo, JSON.stringify(todos.data[todo]))
-      }
+  for(let todo in todos.session){
+    if (!sessionStorage.getItem(todo)) {
+      sessionStorage.setItem(todo, JSON.stringify(todos.session[todo]))
     }
   }
-  if (todos.tData) {
-    for (let todo in todos.tData) {
-      if (!localStorage.getItem(todo) && !sessionStorage.getItem(todo)) {
-        sessionStorage.setItem(todo, JSON.stringify(todos.tData[todo]))
-      }
-    }
+  const storage = {
+    local: {},
+    session: {}
   }
+  for(let local in todos.local){
+    storage.local[local] = dataGet(localStorage.getItem(local))
+  }
+  for(let session in todos.session){
+    storage.session[session] = dataGet(sessionStorage.getItem(session))
+  }
+  console.log(storage.local)
+  console.log(storage.session)
 }
-
-Toast.read = function (key) {
-  if (!window.amx[key]) {
-    keyData(key)
-  }
-  return {
-    data() {
-      return window.amx
-    },
-    created() {
-      this[key] = window.amx[key]
-    },
-    mounted() {
-      window.addEventListener('storage', (e) => {
-        console.log('1')
-        if (e.key == key) {
-          console.log('2')
-          this[e.key] = window.amx[e.key]
-        }
-      })
-    }
-  }
-}
-
-Toast.delete = function (todos) {
-  for (let todo in todos) {
-    localStorage.removeItem(todos[todo])
-    sessionStorage.removeItem(todos[todo])
-  }
-}
-Toast.allDelete = function (type) {
-  if (type === true) {
-    sessionStorage.clear()
-    return
-  } else if (type === false) {
-    localStorage.clear()
-    return
-  }
-  sessionStorage.clear()
-  localStorage.clear()
-}
-export default Toast;
+export default amx_storage_vue
--- src/main.js
+++ src/main.js
@@ -5,15 +5,195 @@
  * @Website: https://senliangpi.github.io/blog/#/
  * @Date: 2020-08-16 17:23:34
  * @LastEditors: Pi Patle
- * @LastEditTime: 2020-10-21 09:59:10
+ * @LastEditTime: 2020-10-21 15:01:30
  */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
-
+const value_list = {
+  local: {
+    aa: '1',
+    a: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    b: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    c: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    d: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    e: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    f:[1,2,3,4,5,6,7,8,9,10],
+    checklist: {
+      status: '',//保存状态 [0: 草稿 1:已发布]
+      carCode: '',//车牌号
+      isHaveReservation: '',//有无预约单
+      reserveBillCode: '',//预约单号
+      clientMobile: '',//客户手机号
+      promiseRepairBegin: '',//保修日期
+      carFuelNum: '',//燃油量
+      carMilageNum: '',//里程数
+      receiveBy: '',//销售顾问
+      clientName: '',//客户姓名
+      carModel: '',//车辆型号
+      vin: '',//VIN
+      returnFactoryType: '',//回厂类别
+      carInsideFeatureButtonCheckItem: { //车辆内部功能以及按键检查
+        airConditionerBlower: '',//空调、鼓风机
+        audioPerformance: '',//音响工作表现
+        glassLift: '',//玻璃升降
+        informationSystem: '',//信息系统
+        steeringWheelFunctionKey: '',//方向盘功能键
+        wiper: '',//雨刷
+        handbrake: '',//手刹
+        instrumentInterior: '',//仪表内饰
+        seatAdjustment: ''//座椅调节、记忆功能
+      },
+      carLiftCheckItem: {
+        brakeDiscLeveling: '',//制动盘表面平整
+        brakePadThicknessAfter: '',//刹车片厚度后
+        brakePadThicknessBefore: '',//刹车片厚度前
+        brakePadsCanContinueToBeUsed: '',//刹车片可继续使用 
+        brakeSystemDamage: '',//制动系统沙漏、变形
+        fuelTankTubing: [],//油箱、油管
+        shockAbsorber: [],//减震器
+        tire: [],//轮胎
+        visibleOilLeakage: [],//可视的漏油漏水
+      },
+      carGoodsCheckItem: {
+        firstFourCarGoods: [],//0:备胎 1:千斤顶 2:随车工具 3:点烟器
+        am: '',
+        cd: '',
+        fm: '',
+      },
+      carSrpMaintainItem: {//车辆SRP保养项
+        oilFilter: 0,
+        frontBrakePad: 0,
+        vehicleInspection: 0,
+        rearBrakePad: 0,
+        frontBrakePadAndBrakeDisc: 0,
+        rearBrakePadAndBrakeDisc: 0,
+        wiperBlade: 0,
+        dustFilter: 0,
+        sparkPlug: 0,
+        fuelFilter: 0,
+        airFilter: 0,
+        brakeFluid: 0,
+      },
+      carClientDeclareItem: {//客户声明项
+        isAgreeToTheRoadTest: '',//如需要，是否同意路试
+        isCarWash: '',//是否洗车
+        isKeepOldParts: '',//是否保留旧件
+        isNeedExtraWork: '',//如需额外工作，如何处理
+        isNoValuables: '',//确认车内无贵重物品
+        notExceedMoneyToNotSure: '',//如费用不超过￥元，可不用再次确认
+        rearBrakePadsAndBrakeDiscs: ''//后部制动片及制动盘
+      },
+      carOutsideCheckImage: '',//车辆外观确认img
+      carOutsideCheckItem: {//车辆外观确认项
+        firstTwoChooseItem: [],//车辆过脏，无法判断
+        leftFrontRimBreak: '',//左前轮辋损伤
+        leftFrontTireTread: '',//左前轮胎花纹
+        leftRearRimBreak: '',//左后轮辋损伤
+        leftRearTireTread: '',//左后轮胎花纹
+        rightFrontRimBreak: '',//右前轮辋损伤
+        rightFrontTireTread: '',//右前轮胎花纹
+        rightRearRimBreak: '',//右后后轮辋损伤
+        rightRearTireTread: '',//右后轮胎花纹
+      },
+      clientSignImage: '',//客户签字确认(image)
+      receiveBySignImage: ''//销售顾问签字确认(image)
+    }
+  },
+  session: {
+    aa: '1',
+    a: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    b: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    c: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    d: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    e: {a:1,b:2,c:3,d:4,e:5,f:6,g:7},
+    f:[1,2,3,4,5,6,7,8,9,10],
+    checklist: {
+      status: '',//保存状态 [0: 草稿 1:已发布]
+      carCode: '',//车牌号
+      isHaveReservation: '',//有无预约单
+      reserveBillCode: '',//预约单号
+      clientMobile: '',//客户手机号
+      promiseRepairBegin: '',//保修日期
+      carFuelNum: '',//燃油量
+      carMilageNum: '',//里程数
+      receiveBy: '',//销售顾问
+      clientName: '',//客户姓名
+      carModel: '',//车辆型号
+      vin: '',//VIN
+      returnFactoryType: '',//回厂类别
+      carInsideFeatureButtonCheckItem: { //车辆内部功能以及按键检查
+        airConditionerBlower: '',//空调、鼓风机
+        audioPerformance: '',//音响工作表现
+        glassLift: '',//玻璃升降
+        informationSystem: '',//信息系统
+        steeringWheelFunctionKey: '',//方向盘功能键
+        wiper: '',//雨刷
+        handbrake: '',//手刹
+        instrumentInterior: '',//仪表内饰
+        seatAdjustment: ''//座椅调节、记忆功能
+      },
+      carLiftCheckItem: {
+        brakeDiscLeveling: '',//制动盘表面平整
+        brakePadThicknessAfter: '',//刹车片厚度后
+        brakePadThicknessBefore: '',//刹车片厚度前
+        brakePadsCanContinueToBeUsed: '',//刹车片可继续使用 
+        brakeSystemDamage: '',//制动系统沙漏、变形
+        fuelTankTubing: [],//油箱、油管
+        shockAbsorber: [],//减震器
+        tire: [],//轮胎
+        visibleOilLeakage: [],//可视的漏油漏水
+      },
+      carGoodsCheckItem: {
+        firstFourCarGoods: [],//0:备胎 1:千斤顶 2:随车工具 3:点烟器
+        am: '',
+        cd: '',
+        fm: '',
+      },
+      carSrpMaintainItem: {//车辆SRP保养项
+        oilFilter: 0,
+        frontBrakePad: 0,
+        vehicleInspection: 0,
+        rearBrakePad: 0,
+        frontBrakePadAndBrakeDisc: 0,
+        rearBrakePadAndBrakeDisc: 0,
+        wiperBlade: 0,
+        dustFilter: 0,
+        sparkPlug: 0,
+        fuelFilter: 0,
+        airFilter: 0,
+        brakeFluid: 0,
+      },
+      carClientDeclareItem: {//客户声明项
+        isAgreeToTheRoadTest: '',//如需要，是否同意路试
+        isCarWash: '',//是否洗车
+        isKeepOldParts: '',//是否保留旧件
+        isNeedExtraWork: '',//如需额外工作，如何处理
+        isNoValuables: '',//确认车内无贵重物品
+        notExceedMoneyToNotSure: '',//如费用不超过￥元，可不用再次确认
+        rearBrakePadsAndBrakeDiscs: ''//后部制动片及制动盘
+      },
+      carOutsideCheckImage: '',//车辆外观确认img
+      carOutsideCheckItem: {//车辆外观确认项
+        firstTwoChooseItem: [],//车辆过脏，无法判断
+        leftFrontRimBreak: '',//左前轮辋损伤
+        leftFrontTireTread: '',//左前轮胎花纹
+        leftRearRimBreak: '',//左后轮辋损伤
+        leftRearTireTread: '',//左后轮胎花纹
+        rightFrontRimBreak: '',//右前轮辋损伤
+        rightFrontTireTread: '',//右前轮胎花纹
+        rightRearRimBreak: '',//右后后轮辋损伤
+        rightRearTireTread: '',//右后轮胎花纹
+      },
+      clientSignImage: '',//客户签字确认(image)
+      receiveBySignImage: ''//销售顾问签字确认(image)
+    }
+  }
+}
+import amx_storage_vue from './amx-storage-vue/src/index';
+Vue.use(amx_storage_vue,value_list);
 
 Vue.config.productionTip = false
 
